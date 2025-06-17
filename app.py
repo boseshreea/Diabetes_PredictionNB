7cccafef20e130b1c40f7d390c92da3c5ef5c9ca
Shreea Bose <115404220+boseshreea@users.noreply.github.com>
2025-06-17 16:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # default to 5000 if not set
+    port = int(os.environ.get('PORT', 10000))  # default to 5000 if not set
     app.run(host='0.0.0.0', port=port)
 
 
